716384341370def3b2af6f760dd340a7e232a4df
Pormiiles <andrelima.santos2370@gmail.com>
2024-02-01 21:55:33 -0300

    Adicionando sessão de FAQ ao site + responsividade

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function nextImage() {
 
     document.getElementById("radio"+count).checked = true;
 }
+
+window.onload = function() {
+    let sections = document.querySelectorAll('.conteudo-quinario-subsecao');
+    sections.forEach(function(section) {
+      section.querySelector('img').addEventListener('click', function() {
+        let paragraph = section.nextElementSibling;
+        if (paragraph.style.display === 'none' || paragraph.style.display === '') {
+          paragraph.style.display = 'block';
+          this.src = "images/icons/icons8-menos-24 (1).png";
+        } else {
+          paragraph.style.display = 'none';
+          this.src = "images/icons/icons8-mais-24 (1).png";
+        }
+      });
+    });
+}
